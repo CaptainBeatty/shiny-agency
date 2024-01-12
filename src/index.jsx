5afa5b75c4935a6eb4af283bcbc887a624d0de4d
CaptainBeatty <juliengrange.dev@gmail.com>
2024-01-12 10:04:55 +0100

    first commit

---
+++ src/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/'
+import Survey from './pages/Survey/'
+import Results from './pages/Results/'
+import Freelances from './pages/Freelances/'
+import Error from './pages/Error/'
+import Header from './components/Header'
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/survey/:questionNumber" element={<Survey />} />
+        <Route path="/results" element={<Results />} />
+        <Route path="/freelances" element={<Freelances />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+        <div>
+            <h1>Acceuil 🧮</h1>
+        </div>
+    )
+}
+
+export default Home
