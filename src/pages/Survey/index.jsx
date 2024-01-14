bea62546559e9ab7053ad9e76917ceecb5c2d203
CaptainBeatty <juliengrange.dev@gmail.com>
2024-01-14 13:41:20 +0100

    call API for survey

--- src/pages/Survey/index.jsx
+++ src/pages/Survey/index.jsx
@@ -1,23 +1,95 @@
 import { useParams, Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import styled from 'styled-components'
+import colors from '../../utils/style/colors'
+import { Loader } from '../../utils/style/Atoms'
+
+
+const SurveyContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+const QuestionTitle = styled.h2`
+  text-decoration: underline;
+  text-decoration-color: ${colors.primary};
+`
+const QuestionContent = styled.span`
+  margin: 30px;
+`
+const LinkWrapper = styled.div`
+  padding-top: 30px;
+  & a {
+    color: black;
+  }
+  & a:first-of-type {
+    margin-right: 20px;
+  }
+`
+
+
+
  
 function Survey() {
     const { questionNumber } = useParams()
     const questionNumberInt = parseInt(questionNumber);
     const backQuestion = questionNumberInt === 1? 1 : questionNumberInt - 1;
     const nextQuestion = questionNumberInt + 1;
+    const [surveyData, setSurveyData] = useState({})
+    const [isDataLoading, setDataLoading] = useState(false)
+    const [error, setError] = useState(null)
+
+    // useEffect(() => {
+    //     // fetchData()
+    //     setDataLoading(true)
+    //     fetch(`http://localhost:8000/survey`).then((response) =>
+    //       response.json().then(({ surveyData }) => {
+    //         setSurveyData(surveyData)
+    //         setDataLoading(false)
+    //       })
+    //     )
+    //   }, [])
+
+    useEffect(() => {
+        async function fetchSurvey() {
+            setDataLoading(true)
+            try {
+                const response = await fetch(`http://localhost:8000/survey`)
+                const { surveyData } = await response.json()
+                setSurveyData(surveyData)
+            }
+            catch(err) {
+                console.log(err)
+                setError(true)
+            }
+            finally {
+                setDataLoading(false)
+            }
+        }
+        fetchSurvey()
+    }, [])
+
+    if (error) {
+        return <span>Oups, il y eu un problème</span>
+    }
  
     return (
-        <div>
-            <h1>Faire le test 🧮</h1>
-            <h2>Question {questionNumber}</h2>
+        <SurveyContainer>
+            <QuestionTitle>Question {questionNumber}</QuestionTitle>
+            {isDataLoading ? (
+                <Loader />
+        ) : (
+        <QuestionContent>{surveyData[questionNumber]}</QuestionContent>
+        )}
+        <LinkWrapper>
             <Link to={`/survey/${backQuestion}`}>précédent</Link>
-              {questionNumberInt === 10? (
-              <Link to={`/results`}>Résultats</Link>
-              ) : (
-              <Link to={`/survey/${nextQuestion}`}>suivant</Link>)
-              }
-        </div>
+            {surveyData[questionNumberInt + 1] ? (
+                <Link to={`/survey/${nextQuestion}`}>Suivant</Link>
+            ) : (
+                <Link to="/results">Résultats</Link>
+            )}
+        </LinkWrapper>
+    </SurveyContainer>
     )
-    
 }
 export default Survey
